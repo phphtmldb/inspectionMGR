af70436fd9debf683da08bf6c6a3e789f7dce801
“phphtmldb” <phphtmldb@gmail.com>
2025-02-08 18:46:15 +0900

    basic_frame_set_done

--- inspectionMGR/urls.py
+++ inspectionMGR/urls.py
@@ -1,22 +1,10 @@
-"""
-URL configuration for inspectionMGR project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='main.html'), name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),  # 로그인, 로그아웃, 비밀번호 변경 기능
+    path('accounts/signup/', include('punchMGR.urls')),  # 회원가입 기능 추가
 ]
